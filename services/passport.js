1d40d95e9702ddeeefcca542be276fcec6e3d3ef
Arth Patel <arth48414@gmail.com>
2020-06-12 00:18:45 -0400

    finishes

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,22 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser( (user, done) => {
+  done(null, user.id);
+
+});
+
+
+passport.deserializeUser( (id, done) => {
+
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+
+});
+
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,7 +29,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        }else {
+
+          new User({ googleId: profile.id }).save()
+          .then(user => done(null, user));
+        }
+
+      });
+      
     }
   )
 );
