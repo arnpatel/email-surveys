b44eb62364d4f2063c696ccba101bf0fb8d90807
Arth Patel <arth48414@gmail.com>
2020-06-12 22:46:30 -0400

    Added billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Landing = () => {
+
+    return (
+
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                Ap Contact!
+            </h1>
+            Collect feedback from your users.
+        </div>
+    );
+};
+
+
+export default Landing;
