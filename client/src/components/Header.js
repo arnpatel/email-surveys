d90a4814d751ab18734a1a21fb8bdde46c266714
Arth Patel <arth48414@gmail.com>
2020-06-12 23:34:39 -0400

    add

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,6 +21,7 @@ class Header extends Component {
                     <li key="3" style={{ margin: '0 10px'}}>
                         Credits: {this.props.auth.credits}
                     </li>,
+                    <li key="4"><a href="/surveys">Dashboard</a></li>,
                 <li key="2"><a href="/api/logout">Logout</a></li>
                 ];
         }
