ed6c158067028828c7698091557558cf1bcb72b1
Arth Patel <arth48414@gmail.com>
2020-06-15 19:34:00 -0400

    Success

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,20 +5,11 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-
-const FIELDS = [
-    { label: 'Survey Title' , name: 'title' },
-    { label: 'Subject Line', name: 'subject'},
-    { label: 'Email Body', name: 'body'},
-    { label: 'Recipient List', name: 'email'}
-
-
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             return (
             <Field key={name} component={SurveyField} type="text" label={label} name={name} />
             );
@@ -47,15 +38,15 @@ function validate(values) {
     const errors = {};
 
 
-    errors.emails = validateEmails(values.emails || '');
+    errors.recipients = validateEmails(values.recipients || '');
 
-     _.each(FIELDS, ({ name }) => {
+     _.each(formFields, ({ name }) => {
         if (!values[name]) {
-            errors[name] = 'You must provide a value.';
+            errors[name] = 'You must provide a value';
         }
     });
 
-
+    
 
 
     return errors;
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -11,10 +11,10 @@ module.exports = (survey) => {
                 <p>Please answe the following question:</p>
                 <p>${survey.body}</p>
                 <div>
-                <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+                <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
                 </div>
                 <div>
-                <a href="${keys.redirectDomain}/api/survey/thanks">No</a>
+                <a href="${keys.redirectDomain}/api/survey/${survey.id}/no">No</a>
                 </div>
                 </div>
                 </body>
