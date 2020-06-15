41824c779fcd815e443477b96652b206a098b008
Arth Patel <arth48414@gmail.com>
2020-06-15 09:57:31 -0400

    success

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,69 @@
+// SurveyForm shows a form for aa user to add input
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
+
+
+const FIELDS = [
+    { label: 'Survey Title' , name: 'title' },
+    { label: 'Subject Line', name: 'subject'},
+    { label: 'Email Body', name: 'body'},
+    { label: 'Recipient List', name: 'email'}
+
+
+];
+
+class SurveyForm extends Component {
+    renderFields() {
+        return _.map(FIELDS, ({ label, name }) => {
+            return (
+            <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
+                {this.renderFields()}
+                <Link to="/surveys" className="red lighten-2 btn-flat white-text">
+                Cancel
+                </Link>
+                <button type="submit" className="teal btn-flat right white-text">
+                Next
+                <i className="material-icons right">done</i>  
+                </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+function validate(values) {
+    const errors = {};
+
+
+    errors.emails = validateEmails(values.emails || '');
+
+     _.each(FIELDS, ({ name }) => {
+        if (!values[name]) {
+            errors[name] = 'You must provide a value.';
+        }
+    });
+
+
+
+
+    return errors;
+
+}
+
+export default reduxForm({
+    validate, 
+    form: 'surveyForm',
+    destroyOnUnmount: false
+})(SurveyForm);
---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,26 @@
+const keys = require('../../config/keys');
+
+
+module.exports = (survey) => {
+
+    return `
+        <html>
+                <body>
+                <div style ="text-align: center;">
+                <h3>I'd like your input!</h3>
+                <p>Please answe the following question:</p>
+                <p>${survey.body}</p>
+                <div>
+                <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+                </div>
+                <div>
+                <a href="${keys.redirectDomain}/api/survey/thanks">No</a>
+                </div>
+                </div>
+                </body>
+
+        </html>
+
+    `;
+
+};
