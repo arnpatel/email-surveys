ad8d68defec3f6fcae1dfff5a84b2fd7f10199ba
Arth Patel <arth48414@gmail.com>
2020-06-16 09:21:03 -0400

    s

--- services/Mailer.js
+++ services/Mailer.js
@@ -9,7 +9,7 @@ class Mailer extends helper.Mail {
 
 
     this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email('marketing@arthnpatel.com');
+    this.from_email = new helper.Email('webmaster@arthnpatel.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
