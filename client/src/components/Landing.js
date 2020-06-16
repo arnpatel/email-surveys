cae81b5ae8943403484ae51d5a5e3391072e9d9e
Arth Patel <arth48414@gmail.com>
2020-06-16 11:24:44 -0400

    s

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -26,14 +26,14 @@ const Landing = () => {
                <p><i>Send emails in bulk that reach your users.</i></p>
                <p><i>Contact our team to get started with some custom templates for your business.</i></p>
                <p><i>More contacts, more customers.</i></p>
-               <p><i>Track your services using the End-User's reviews.</i></p>
+               <p><i>Track your services using the End-User's reviews on your products.</i></p>
                <p><i>Email marketing service that is affordable, easy to use, faster, well, and funner.</i></p>
                <p><i>We help your business reach more customers.</i></p>
 
             <h5><u>Pricing:</u></h5>
-            <p><i>We understand your needs, No-contract, Only prepaid service to bring more accessibility.</i></p>
-            <p><i>1 Survey - 1 Dollar - unlimited receipent attachment</i></p>
-            <p><i>Minimum Purchase - Dollar 5</i></p>
+            <p><i>We understand your needs, No-contract, Prepaid service to bring more accessibility.</i></p>
+            <p><i>1 Dollar a survey - Unlimited receipent attachment.</i></p>
+            <p><i>Dollar 5 - Minimum purcahse value.</i></p>
             <p><i>Contact us to place a larger one time purchase order.</i></p>
 
             <h5><u>Contact Us:</u></h5>
