9368acd8fe901662199b9784f473ecd9fc60ccb8
Arth Patel <arth48414@gmail.com>
2020-06-15 21:53:24 -0400

    success

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,15 +1,45 @@
 import React from 'react';
 
+//document.body.style.background = "url('https://edgewater-digital.com/wp-content/uploads/2018/10/Search-Engine-Marketing-Background.jpg')";
+
+
 const Landing = () => {
 
     return (
 
-        <div style={{ textAlign: 'center' }}>
-            <h1>
-                Ap Contact!
+        document.body.style.background = "url('https://edgewater-digital.com/wp-content/uploads/2018/10/Search-Engine-Marketing-Background.jpg')",
+            
+        <div style={{ textAlign: 'center' }} className="white-text">
+            <h1 style={{color: '#009999'}}>
+                AP Contact
             </h1>
-            Collect feedback from your users.
+            <h4>
+                Powerful Email Marketing Made Easy.
+            </h4>
+
+            <div>
+                <h5><u>
+            Application features:
+            </u></h5>
+            
+               <p><i>Send emails in bulk that reach your users.</i></p>
+               <p><i>Contact our team to get started with some custom templates for your business.</i></p>
+               <p><i>More contacts, more customers.</i></p>
+               <p><i>Track your services using the End-User's reviews.</i></p>
+               <p><i>Email marketing service that is affordable, easy to use, faster, well, and funner.</i></p>
+               <p><i>We help your business reach more customers.</i></p>
+
+            <h5><u>Pricing:</u></h5>
+            <p><i>We understand your needs, No-contract, Only prepaid service to bring more accessibility.</i></p>
+            <p><i>1 Survey - 1 Dollar - unlimited receipent attachment</i></p>
+            <p><i>Minimum Purchase - Dollar 5</i></p>
+            <p><i>Contact us to make larger one time purchases</i></p>
+
         </div>
+        
+        </div>
+
+        
     );
 };
 
