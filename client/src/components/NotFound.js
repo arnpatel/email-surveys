19ae8ccd8ab90e5f21dc8df1b3db3fe50fb2f883
Arth Patel <arth48414@gmail.com>
2020-06-18 21:23:14 -0400

    s

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -5,13 +5,11 @@ const NotFound = () => {
     return (
 
 
-        document.body.style.background = "black",
-        <div style={{ textAlign: 'center' }} className="white-text black" >
-            
-              <img src="logo.png" alt="" class="circle responsive-img">
-            </img>
+        document.body.style.background = "#e0f2f1",
+        <div style={{ textAlign: 'center' }} className="black-text #e0f2f1" >
+ 
 
-            <h1 style={{color: 'white'}}><b>
+            <h1 style={{color: 'black'}}><b>
     
                 Page not found. 
                 <h5>The page you requested was not found on this server.</h5>
@@ -19,10 +17,45 @@ const NotFound = () => {
                 <h6>The URL may have been mistyped or you may have a old link. We apologize for the inconvenience.</h6>
                 </b>
             </h1>
+
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <div class="container" className="cyan darken-3 white-text">
+                <br />
+                <a href='/privacy-policy' className="black-text">Privacy Policy</a>   -   Copyright ©2020 Developed By Arth Patel    -  <a href='/terms-and-conditions' className="black-text">Terms And Conditions.</a>
+                <br />
+                <br />
+                </div>
+
+
+
+
             </div>
+
+
+            
 
         );
 };
+
+
 
 
 
