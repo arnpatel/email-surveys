be53b23faba49966f951145391577d2a0ee47df1
Arth Patel <arth48414@gmail.com>
2020-06-22 23:57:50 -0400

    s

--- client/src/components/JoinUs.js
+++ client/src/components/JoinUs.js
@@ -26,7 +26,7 @@ const JoinUs = () => {
           <br />
           <br />
           <h6> Benjamin Franklin once said ‘Tell me and I forget. Teach me and I remember. Involve me and I learn.’</h6> 
-              <h6>We are as transparent as glass. We work with NodeJS, React, Redux, Express, MongoDB, and several other Front-end and Back-end framework and libraries to bring Full-stack development of this website. </h6>
+              <h6>We are as transparent as glass. We work with NodeJS, React, Redux, Express, MongoDB, and other Front-end and Back-end framework and libraries to bring Full-stack development of this website. </h6>
                   <h6>Should you think you might be a good fit, Please fill out the form or contact us at <a href="mailto:developer@arthnpatel.com">developer@arthnpatel.com</a>.</h6>
           
             <br />
@@ -35,7 +35,7 @@ const JoinUs = () => {
             <br />
             <br />
             <br />
-            <p>* We currently only search for college student.</p>
+            <p>* We currently only search for college students.</p>
             <div class="container" className="cyan darken-3 white-text">
                 <br />
             <a href='/privacy-policy' className="black-text">Privacy Policy</a>   -   Copyright ©2020 AP Contact Team    -  <a href='/terms-and-conditions' className="black-text">Terms And Conditions.</a>
